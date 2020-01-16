27f134c2dd96a75656deb98d1f0795def394559f
DoKe65 <aurelia65@gmail.com>
2020-01-16 16:41:05 +0100

    Adjustments to targetted url

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
+// Aus Produktseite des Shops:
+const btnVorschau = document.getElementById("btnVorschau");
+const file = btnVorschau.getAttribute("data-file");
+const title = btnVorschau.getAttribute("data-title");
+
 const overlay = document.createElement("div");
 const bookTitle = document.createElement("h1");
 const lightbox = document.createElement("div");
 const iFrame = document.createElement("iframe");
-const button = document.getElementById("btnVorschau");
 const closeLightbox = document.createElement("button");
+
+
+function showLightbox(file, title) {
+
+  btnVorschau.addEventListener("click", (e) => {
+
+    overlay.setAttribute("id", "overlay");
+    overlay.style.display = "block";
+
+    lightbox.setAttribute("id", "lightbox");
+
+    iFrame.setAttribute("src", "https://kursunterlagen.ch/leseproben/ECDL_BB_GIMP28/");
+    iFrame.setAttribute("allowfullscreen", "true");
+
+    bookTitle.classList.add("book_title");
+    bookTitle.innerHTML = "ECDL Bildbearbeitung mit GIMP 2.8";
+
+    closeLightbox.setAttribute("id", "closeLightbox");
+    closeLightbox.innerHTML = "X";
+    closeLightbox.addEventListener("click", (e) => {
+      document.body.removeChild(overlay);
+    });
+
+    lightbox.appendChild(bookTitle);
+    lightbox.appendChild(closeLightbox);
+    lightbox.appendChild(iFrame);
+    overlay.appendChild(lightbox);
+    document.body.appendChild(overlay);
+
+  });
+}
 
 
 btnVorschau.addEventListener("click", (e) => {
@@ -13,7 +48,7 @@ btnVorschau.addEventListener("click", (e) => {
 
   lightbox.setAttribute("id", "lightbox");
 
-  iFrame.setAttribute("src", "https://kursunterlagen.ch/leseproben/ECDL_BB_GIMP28/LP_ECDL_BB_GIMP28.html");
+  iFrame.setAttribute("src", "https://kursunterlagen.ch/leseproben/ECDL_BB_GIMP28/");
   iFrame.setAttribute("allowfullscreen", "true");
 
   bookTitle.classList.add("book_title");
