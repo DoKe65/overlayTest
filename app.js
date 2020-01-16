5e700dbbcc0b00933349b9bec281a1b0f5d127ed
DoKe65 <aurelia65@gmail.com>
2020-01-16 16:59:11 +0100

    hardcodes for testing

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // Aus Produktseite des Shops:
+const btnVorschau = document.getElementById("btnVorschau");
 // Im Produkt muss für den Button die entsprechenden data- Attributte
 // gesetzt werden, z. B. data-url="kursunterlagen/leseproben/LP_ECDL_BB"
 // und data-title="ECDL Bidlbearbeitung mit GIMP 2.8"
-const btnVorschau = document.getElementById("btnVorschau");
-const url = btnVorschau.getAttribute("data-url");
-const title = btnVorschau.getAttribute("data-title");
+// const url = btnVorschau.getAttribute("data-url");
+// const title = btnVorschau.getAttribute("data-title");
+
+// temporarily hardcoded for testing
+const url = "https://kursunterlagen.ch/leseproben/ECDL_BB_GIMP28/";
+const title = "ECDL Bildbearbeitung mit GIMP 2.8";
 
 const overlay = document.createElement("div");
 const bookTitle = document.createElement("h1");
@@ -43,30 +47,31 @@ function showLightbox(url, title) {
   });
 }
 
+showLightbox(url, title);
 
-btnVorschau.addEventListener("click", (e) => {
-
-  overlay.setAttribute("id", "overlay");
-  overlay.style.display = "block";
-
-  lightbox.setAttribute("id", "lightbox");
-
-  iFrame.setAttribute("src", "https://kursunterlagen.ch/leseproben/ECDL_BB_GIMP28/");
-  iFrame.setAttribute("allowfullscreen", "true");
-
-  bookTitle.classList.add("book_title");
-  bookTitle.innerHTML = "ECDL Bildbearbeitung mit GIMP 2.8";
-
-  closeLightbox.setAttribute("id", "closeLightbox");
-  closeLightbox.innerHTML = "X";
-  closeLightbox.addEventListener("click", (e) => {
-    document.body.removeChild(overlay);
-  });
-
-  lightbox.appendChild(bookTitle);
-  lightbox.appendChild(closeLightbox);
-  lightbox.appendChild(iFrame);
-  overlay.appendChild(lightbox);
-  document.body.appendChild(overlay);
-
-});
+// btnVorschau.addEventListener("click", (e) => {
+//
+//   overlay.setAttribute("id", "overlay");
+//   overlay.style.display = "block";
+//
+//   lightbox.setAttribute("id", "lightbox");
+//
+//   iFrame.setAttribute("src", "https://kursunterlagen.ch/leseproben/ECDL_BB_GIMP28/");
+//   iFrame.setAttribute("allowfullscreen", "true");
+//
+//   bookTitle.classList.add("book_title");
+//   bookTitle.innerHTML = "ECDL Bildbearbeitung mit GIMP 2.8";
+//
+//   closeLightbox.setAttribute("id", "closeLightbox");
+//   closeLightbox.innerHTML = "X";
+//   closeLightbox.addEventListener("click", (e) => {
+//     document.body.removeChild(overlay);
+//   });
+//
+//   lightbox.appendChild(bookTitle);
+//   lightbox.appendChild(closeLightbox);
+//   lightbox.appendChild(iFrame);
+//   overlay.appendChild(lightbox);
+//   document.body.appendChild(overlay);
+//
+// });
